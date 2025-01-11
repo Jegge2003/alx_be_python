0f32079ebce1e3db1899fc72ce0b64f3fd09416c
Jegge2003 <jamesoappiah2003@gmail.com>
2025-01-11 23:07:06 +0000

    Edited temp conversion file

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,12 +3,12 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 #Function to convert Fahrenheit to Celsius
-def convert_to_celsius(fahrenheit):
-    return FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
+def convert_to_celsius(Fahrenheit):
+    return (Fahrenheit - 32 ) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 #Function to convert Celsius to Fahrenheit
-def convert_to_fahrenheit(celsius):
-    return (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
+def convert_to_fahrenheit(Celsius):
+    return (Celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 #Main script logic
 def main():
