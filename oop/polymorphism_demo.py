687c8d6819f4404629e622dec60091cd9c1e5e57
Jegge2003 <jamesoappiah2003@gmail.com>
2025-01-26 15:49:16 +0000

    Added polymorphism demo file

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,20 @@
+class Shape:
+    def area(self):
+        raise NotImplementedError('Subclass must implement abstract method')
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        """Calculating area of the rectangle"""
+        return self.length * self.width
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        """Calculating area of the circle"""
+        return math.pi * (self.radius ** 2)
