e415683d0f93a4181a3d31e44d7b64105c494038
Jegge2003 <jamesoappiah2003@gmail.com>
2025-01-26 18:38:41 +0000

    Make some modification

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -5,10 +5,10 @@ class Calculator:
     #A static method returning the sum of two number a and b
     @staticmethod
     def add(a, b):
-        return a+b
+        return a + b
     
     #A class method returning the product of two numbers a and b
     @classmethod
     def multiply(cls, a, b):
         print(f"Calculation type: {cls.calculation_type}")
-        return a*b
+        return a * b
